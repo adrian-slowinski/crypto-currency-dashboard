486ccdddfb2ff5d84383ef0b8184bb2f54b45cbb
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-04-20 16:52:22 +0200

    Add Coin Tile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -2,17 +2,24 @@ import React from 'react';
 import styled, {css} from 'styled-components';
 import { AppContext } from '../App/AppProvider';
 import {Tile, SelectableTile} from '../Shared/Tile'
+import CoinTile from './CoinTile';
  
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(6, 1fr);
+  grid-template-columns: repeat(5, 1fr);
   grid-gap: 15px;
 `
+function getCoinsToDisplay(coinList) {
+  return Object.keys(coinList).slice(0, 250);
+}
+
 export default function () {
   return <AppContext.Consumer> 
     {({coinList}) => <CoinGridStyled> 
-      {Object.keys(coinList).map(coinKey => <SelectableTile>{coinKey}</SelectableTile>)}
+      {getCoinsToDisplay(coinList).map(coinKey =>
+          <CoinTile key={coinKey}   coinKey={coinKey}/>
+        )}
     </CoinGridStyled> }
   </AppContext.Consumer>
 }
