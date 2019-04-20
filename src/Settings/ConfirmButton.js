526dd53fc313113b24e17d2fba6534c61cc7e46a
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-04-20 16:10:56 +0200

    Create coin grid

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from '../App/AppProvider';
+import {fontSize1, greenBoxShadow, color3} from '../Shared/Styles'
 
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
-  color: yellowgreen;
+  color: ${color3};
+  ${fontSize1};
+  padding: 2px 5px;
   cursor: pointer;
+  &:hover{
+    ${greenBoxShadow}
+  }
 `
 const CenterDiv = styled.div`
   display: grid;
