b338281f3c8383d4467b8703d237dfb6a0431665
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-04-23 10:16:26 +0200

    Reponsive grid

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -7,7 +7,7 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat( auto-fill, minmax(100px, 1fr) );
   grid-gap: 15px;
 `
 function getCoinsToDisplay(coinList, topSection, favorites) {
